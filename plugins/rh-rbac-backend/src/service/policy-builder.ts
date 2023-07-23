3f75def31c6fcb9a54bf7f5de573442fb76f9385
Patrick Knight <pknight@redhat.com>
2023-07-23 22:58:14 +0300

    feat(rbac): implement initial CRUD API for permission policies
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- plugins/rh-rbac-backend/src/service/policy-builder.ts
+++ plugins/rh-rbac-backend/src/service/policy-builder.ts
@@ -14,19 +14,39 @@ import {
 import {
   AuthorizeResult,
   PermissionEvaluator,
+  QueryPermissionRequest,
 } from '@backstage/plugin-permission-common';
 import { createPermissionIntegrationRouter } from '@backstage/plugin-permission-node';
 
-import { FileAdapter } from 'casbin';
+import { FileAdapter, newEnforcer, newModelFromString } from 'casbin';
 import { Router } from 'express';
 import { Logger } from 'winston';
 
 import {
+  policyEntityCreatePermission,
+  policyEntityDeletePermission,
   policyEntityPermissions,
   policyEntityReadPermission,
   RESOURCE_TYPE_POLICY_ENTITY,
 } from '../permissions';
 import { RBACPermissionPolicy } from './permission-policy';
+
+export const MODEL = `
+[request_definition]
+r = sub, obj, act
+
+[policy_definition]
+p = sub, obj, act, eft
+
+[policy_effect]
+e = some(where (p.eft == allow)) && !some(where (p.eft == deny))
+
+[role_definition]
+g = _, _
+
+[matchers]
+m = r.sub == p.sub && r.obj == p.obj && r.act == p.act
+`;
 
 export class PolicyBuilder {
   public static async build(env: {
@@ -37,7 +57,7 @@ export class PolicyBuilder {
     permissions: PermissionEvaluator;
   }): Promise<Router> {
     // TODO: Replace with a DB adapter.
-    const fileAdapter = new FileAdapter(
+    const adapter = new FileAdapter(
       resolvePackagePath(
         '@janus-idp/plugin-rh-rbac-backend',
         './model/rbac-policy.csv',
@@ -46,6 +66,23 @@ export class PolicyBuilder {
 
     const permissions = env.permissions;
 
+    const theModel = newModelFromString(MODEL);
+    const enforcer = await newEnforcer(theModel, adapter);
+
+    const authorize = async (
+      authHeader: string | undefined,
+      permissionEvaluator: PermissionEvaluator,
+      permission: QueryPermissionRequest,
+    ) => {
+      const token = getBearerTokenFromAuthorizationHeader(authHeader);
+
+      const decision = (
+        await permissionEvaluator.authorizeConditional([permission], { token })
+      )[0];
+
+      return decision;
+    };
+
     const options: RouterOptions = {
       config: env.config,
       logger: env.logger,
@@ -53,8 +90,8 @@ export class PolicyBuilder {
       identity: env.identity,
       policy: await RBACPermissionPolicy.build(
         env.logger,
-        fileAdapter,
         env.config,
+        enforcer,
       ),
     };
 
@@ -68,24 +105,109 @@ export class PolicyBuilder {
     router.use(permissionsIntegrationRouter);
 
     router.get('/', async (request, response) => {
-      const token = getBearerTokenFromAuthorizationHeader(
+      const decision = await authorize(
         request.header('authorization'),
+        permissions,
+        { permission: policyEntityReadPermission },
       );
-
-      const decision = (
-        await permissions.authorizeConditional(
-          [{ permission: policyEntityReadPermission }],
-          {
-            token,
-          },
-        )
-      )[0];
 
       if (decision.result === AuthorizeResult.DENY) {
         response.status(403);
         response.send({ status: 'Unauthorized' });
       } else {
         response.send({ status: 'Authorized' });
+      }
+    });
+
+    router.get('/policies', async (request, response) => {
+      const decision = await authorize(
+        request.header('authorization'),
+        permissions,
+        { permission: policyEntityReadPermission },
+      );
+
+      if (decision.result === AuthorizeResult.DENY) {
+        response.status(403);
+        response.send({ status: 'Unauthorized' });
+      } else {
+        const policies = await enforcer.getPolicy();
+        response.json(policies);
+      }
+    });
+
+    router.get('/policy/:namespace/:id', async (request, response) => {
+      const str = request.params.namespace.concat('/');
+      const user = str + request.params.id;
+
+      const decision = await authorize(
+        request.header('authorization'),
+        permissions,
+        { permission: policyEntityReadPermission },
+      );
+
+      if (decision.result === AuthorizeResult.DENY) {
+        response.status(403);
+        response.send({ status: 'Unauthorized' });
+      } else {
+        const policy = await enforcer.getFilteredPolicy(0, user);
+        if (!(policy.length === 0)) {
+          response.json(policy);
+        } else {
+          response.status(404).end();
+        }
+      }
+    });
+
+    router.delete('/policy', async (request, response) => {
+      const policy = request.body;
+
+      const decision = await authorize(
+        request.header('authorization'),
+        permissions,
+        { permission: policyEntityDeletePermission },
+      );
+
+      if (decision.result === AuthorizeResult.DENY) {
+        response.status(403);
+        response.send({ status: 'Unauthorized' });
+      } else {
+        const policyPermission = [
+          policy.user,
+          policy.permission,
+          policy.policy,
+          policy.effect,
+        ];
+        await enforcer.removePolicy(...policyPermission);
+        response.status(200).end();
+      }
+    });
+
+    router.post('/policy', async (request, response) => {
+      const policy = request.body;
+
+      const decision = await authorize(
+        request.header('authorization'),
+        permissions,
+        { permission: policyEntityCreatePermission },
+      );
+
+      if (decision.result === AuthorizeResult.DENY) {
+        response.status(403);
+        response.send({ status: 'Unauthorized' });
+      } else {
+        const policyPermission = [
+          policy.user,
+          policy.permission,
+          policy.policy,
+          policy.effect,
+        ];
+
+        if (!(await enforcer.hasPolicy(...policyPermission))) {
+          await enforcer.addPolicy(...policyPermission);
+          response.status(201).end();
+        } else {
+          response.status(409).end();
+        }
       }
     });
 
